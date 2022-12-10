5a1a60eb8e69aac04c2681822b302a2e3f025a0e
puranchandralohar <clpuran95@gmail.com>
2022-12-10 22:33:46 +0530

    Added

---
+++ mui/src/components/Typo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Typography } from '@mui/material'
+
+export const Typo = () => {
+  return (
+    <div>
+        <Typography variant='h1' color="primary">Hello</Typography>
+        <Typography variant='h2'>Hello</Typography>
+        <Typography variant='h3'>Hello</Typography>
+        <Typography variant='h4'>Hello</Typography>
+        <Typography variant='h5'>Hello</Typography>
+        <Typography variant='h6'>Hello</Typography>
+    </div>
+  )
+}
+
